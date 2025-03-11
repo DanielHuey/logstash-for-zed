dccd55860ce5b14f4f1c10651b18495cac30104a
DanielHuey <adhueywilan@gmail.com>
2025-03-11 12:47:42 +0300

    first commit

---
+++ languages/logstash/highlights.scm
@@ -0,0 +1,62 @@
+; This file tells tree‐sitter which nodes get which highlight scopes.
+;
+; The names on the right (e.g. @keyword, @comment, @string) are used by the
+; editor/theme to style these parts of the code.
+;
+; Customize these patterns as needed.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Section keywords
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(input_section) @keyword
+(filter_section) @keyword
+(output_section) @keyword
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Punctuation and Operators
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Brackets:
+[
+ "[" "]"
+ "{" "}"
+] @punctuation.bracket
+
+; The operator in expressions:
+"=>" @operator
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Literals
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(string) @string
+(number) @number
+(boolean) @constant
+
+; Arrays are highlighted as constants, but you could also choose another group:
+(array) @constant
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Identifiers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Plugin names when used as identifiers:
+(plugin_name) @variable
+
+; Expression keys and values:
+(expression_key) @variable
+(expression_value) @literal
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Structural Nodes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Highlight blocks as regions:
+(block) @region
+
+; An expression rule (which in this language connects keys and values):
+; (expression) @operator
+
+; Comments:
+(comment) @comment
